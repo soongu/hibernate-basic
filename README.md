c3ef543e4d5a7cc3da9aa85afc6f1c7ce06918b7
soongu <hsg9984@gmail.com>
2022-12-16 12:42:48 +0900

    # 1. 하이버네이트, h2데이터베이스 설정

---
+++ README.md
@@ -0,0 +1,6 @@
+# JPA BASIC 
+
+## 초기설정
+- [h2 다운로드 링크](http://www.h2database.com/html/download.html)
+- h2 콘솔로 들어가서 Generic H2 (server)설정에서 JDBC URL `jdbc:h2:~/test`로 설정 -> 연결
+- 그 이후부터는 `jdbc:h2:tcp://localhost/~/test` URL 사용
---
+++ src/main/java/hellojpa/Main.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기서 hello는 persistence.xml에서 정한 unit name
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // code
+        Member member = new Member(1L, "helloA");
+
+        em.persist(member);
+
+        tx.commit();
+
+        em.close();
+        emf.close();
+    }
+}
