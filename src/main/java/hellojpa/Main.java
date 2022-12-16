471076dbb428b7c63c52963112a2805e02fd7d22
soongu <hsg9984@gmail.com>
2022-12-16 14:44:37 +0900

    #4. Entity클래스로 DDL 생성하기

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -18,12 +18,12 @@ public class Main {
 
         try {
 
-            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(10)
-                    .getResultList();
-
-            resultList.forEach(System.out::println);
+//            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            resultList.forEach(System.out::println);
 
             tx.commit();
         } catch (Exception e) {
